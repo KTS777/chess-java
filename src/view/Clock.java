03ea653b691308bc195d38db462a7e13c7da1225
KTS777 <ketitsinadze23@gmail.com>
2025-04-15 00:53:56 +0400

    Refactored Clock and StartMenu classes

--- src/view/Clock.java
+++ src/view/Clock.java
@@ -4,33 +4,36 @@ public class Clock {
     private int hh;
     private int mm;
     private int ss;
-    
+
     public Clock(int hh, int mm, int ss) {
         this.hh = hh;
         this.mm = mm;
         this.ss = ss;
     }
-    
+
     public boolean outOfTime() {
-        return (hh == 0 && mm == 0 && ss == 0);
+        return hh == 0 && mm == 0 && ss == 0;
     }
-    
+
     public void decr() {
-        if (this.mm == 0 && this.ss == 0) {
-            this.ss = 59;
-            this.mm = 59;
-            this.hh--;
-        } else if (this.ss == 0) {
-            this.ss = 59;
-            this.mm--;
-        } else this.ss--;
+        if (mm == 0 && ss == 0) {
+            ss = 59;
+            mm = 59;
+            hh--;
+        } else if (ss == 0) {
+            ss = 59;
+            mm--;
+        } else {
+            ss--;
+        }
     }
-    
+
     public String getTime() {
-        String fHrs = String.format("%02d", this.hh);
-        String fMins = String.format("%02d", this.mm);
-        String fSecs = String.format("%02d", this.ss);
-        String fTime = fHrs + ":" + fMins + ":" + fSecs;
-        return fTime;
+        return String.format("%02d:%02d:%02d", hh, mm, ss);
+    }
+
+    @Override
+    public String toString() {
+        return getTime();
     }
 }
--- src/view/StartMenu.java
+++ src/view/StartMenu.java
@@ -18,138 +18,162 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class StartMenu implements Runnable {
+
+    private static final String BLACK_PAWN_IMG = "bp.png";
+    private static final String WHITE_PAWN_IMG = "wp.png";
+    private static final String[] HOURS = {"0", "1", "2", "3"};
+    private static final int MAX_TIME_UNIT = 60;
+
     public void run() {
         final JFrame startWindow = new JFrame("Chess");
-        // Set window properties
-        startWindow.setLocation(300,100);
+        startWindow.setLocation(300, 100);
         startWindow.setResizable(false);
         startWindow.setSize(260, 240);
-        
+
         Box components = Box.createVerticalBox();
         startWindow.add(components);
-        
-        // view.Game title
-        final JPanel titlePanel = new JPanel();
-        components.add(titlePanel);
-        final JLabel titleLabel = new JLabel("Chess");
-        titlePanel.add(titleLabel);
-        
-        // Black player selections
-        final JPanel blackPanel = new JPanel();
-        components.add(blackPanel, BorderLayout.EAST);
-        final JLabel blackPiece = new JLabel();
-        try {
-            Image blackImg = ImageIO.read(getClass().getResource("bp.png"));
-            blackPiece.setIcon(new ImageIcon(blackImg));
-            blackPanel.add(blackPiece);
-        } catch (Exception e) {
-            System.out.println("Required game file bp.png missing");
-        }
-        
-        
-        
+
+        addTitlePanel(components);
+
         final JTextField blackInput = new JTextField("Black", 10);
-        blackPanel.add(blackInput);
-        
-        // White player selections
-        final JPanel whitePanel = new JPanel();
-        components.add(whitePanel);
-        final JLabel whitePiece = new JLabel();
-        
-        try {
-            Image whiteImg = ImageIO.read(getClass().getResource("wp.png"));
-            whitePiece.setIcon(new ImageIcon(whiteImg));
-            whitePanel.add(whitePiece);
-            startWindow.setIconImage(whiteImg);
-        }  catch (Exception e) {
-            System.out.println("Required game file wp.png missing");
-        }
-        
-        
+        components.add(createPlayerPanel("Black", BLACK_PAWN_IMG, blackInput), BorderLayout.EAST);
+
         final JTextField whiteInput = new JTextField("White", 10);
-        whitePanel.add(whiteInput);
-        
-        // Timer settings
-        final String[] minSecInts = new String[60];
-        for (int i = 0; i < 60; i++) {
-            if (i < 10) {
-                minSecInts[i] = "0" + Integer.toString(i);
-            } else {
-                minSecInts[i] = Integer.toString(i);
-            }
-        }
-        
-        final JComboBox<String> seconds = new JComboBox<String>(minSecInts);
-        final JComboBox<String> minutes = new JComboBox<String>(minSecInts);
-        final JComboBox<String> hours = 
-                new JComboBox<String>(new String[] {"0","1","2","3"});
-        
-        Box timerSettings = Box.createHorizontalBox();
-        
-        hours.setMaximumSize(hours.getPreferredSize());
-        minutes.setMaximumSize(minutes.getPreferredSize());
-        seconds.setMaximumSize(minutes.getPreferredSize());
-        
-        timerSettings.add(hours);
-        timerSettings.add(Box.createHorizontalStrut(10));
-        timerSettings.add(seconds);
-        timerSettings.add(Box.createHorizontalStrut(10));
-        timerSettings.add(minutes);
-        
-        timerSettings.add(Box.createVerticalGlue());
-        
-        components.add(timerSettings);
-        
-        // Buttons
+        JPanel whitePanel = createPlayerPanel("White", WHITE_PAWN_IMG, whiteInput);
+        components.add(whitePanel);
+
+        ImageIcon icon = (ImageIcon) ((JLabel) whitePanel.getComponent(0)).getIcon();
+        if (icon != null) startWindow.setIconImage(icon.getImage());
+
+        final JComboBox<String> hours = createTimeComboBox(HOURS);
+        final JComboBox<String> minutes = createTimeComboBox(generateTimeUnits());
+        final JComboBox<String> seconds = createTimeComboBox(generateTimeUnits());
+        components.add(setupTimerSettings(hours, minutes, seconds));
+
+        components.add(createControlButtons(startWindow, blackInput, whiteInput, hours, minutes, seconds));
+        components.add(Box.createGlue());
+
+        startWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startWindow.setVisible(true);
+    }
+
+
+    private Box createControlButtons(JFrame startWindow,
+                                     JTextField blackInput,
+                                     JTextField whiteInput,
+                                     JComboBox<String> hours,
+                                     JComboBox<String> minutes,
+                                     JComboBox<String> seconds) {
         Box buttons = Box.createHorizontalBox();
-        final JButton quit = new JButton("Quit");
-        
-        quit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-              startWindow.dispose();
-            }
-          });
-        
-        final JButton instr = new JButton("Instructions");
-        
-        instr.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(startWindow,
-                        "To begin a new game, input player names\n" +
-                        "next to the pieces. Set the clocks and\n" +
-                        "click \"Start\". Setting the timer to all\n" +
-                        "zeroes begins a new untimed game.",
-                        "How to play",
-                        JOptionPane.PLAIN_MESSAGE);
-            }
-          });
-        
-        final JButton start = new JButton("Start");
-        
-        start.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String bn = blackInput.getText();
-                String wn = whiteInput.getText();
-                int hh = Integer.parseInt((String) hours.getSelectedItem());
-                int mm = Integer.parseInt((String) minutes.getSelectedItem());
-                int ss = Integer.parseInt((String) seconds.getSelectedItem());
-                
-                new GameWindow(bn, wn, hh, mm, ss);
-                startWindow.dispose();
-            }
-          });
-        
+
+        JButton quit = new JButton("Quit");
+        quit.addActionListener(e -> startWindow.dispose());
+
+        JButton instr = new JButton("Instructions");
+        instr.addActionListener(e -> showInstructionsDialog(startWindow));
+
+        JButton start = new JButton("Start");
+        start.addActionListener(e -> handleStartClick(
+                blackInput, whiteInput, hours, minutes, seconds, startWindow));
+
         buttons.add(start);
         buttons.add(Box.createHorizontalStrut(10));
         buttons.add(instr);
         buttons.add(Box.createHorizontalStrut(10));
         buttons.add(quit);
-        components.add(buttons);
-        
-        Component space = Box.createGlue();
-        components.add(space);
 
-        startWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        startWindow.setVisible(true);
+        return buttons;
     }
+
+    private void handleStartClick(JTextField blackInput,
+                                  JTextField whiteInput,
+                                  JComboBox<String> hours,
+                                  JComboBox<String> minutes,
+                                  JComboBox<String> seconds,
+                                  JFrame startWindow) {
+        String bn = blackInput.getText();
+        String wn = whiteInput.getText();
+        int hh = Integer.parseInt((String) hours.getSelectedItem());
+        int mm = Integer.parseInt((String) minutes.getSelectedItem());
+        int ss = Integer.parseInt((String) seconds.getSelectedItem());
+
+        new GameWindow(bn, wn, hh, mm, ss);
+        startWindow.dispose();
+    }
+
+
+    private void addTitlePanel(Box container) {
+        JPanel titlePanel = new JPanel();
+        JLabel titleLabel = new JLabel("Chess");
+        titlePanel.add(titleLabel);
+        container.add(titlePanel);
+    }
+
+
+    private void showInstructionsDialog(JFrame parent) {
+        JOptionPane.showMessageDialog(parent,
+                "To begin a new game, input player names\n" +
+                        "next to the pieces. Set the clocks and\n" +
+                        "click \"Start\". Setting the timer to all\n" +
+                        "zeroes begins a new untimed game.",
+                "How to play",
+                JOptionPane.PLAIN_MESSAGE);
+    }
+
+
+    private JPanel createPlayerPanel(String labelText, String imagePath, JTextField nameField) {
+        JPanel panel = new JPanel();
+        JLabel pieceIcon = createPieceLabel(imagePath);
+        panel.add(pieceIcon);
+        panel.add(nameField);
+        return panel;
+    }
+
+
+    private Box setupTimerSettings(JComboBox<String> hours,
+                                   JComboBox<String> minutes,
+                                   JComboBox<String> seconds) {
+
+        Box timerSettings = Box.createHorizontalBox();
+
+        hours.setMaximumSize(hours.getPreferredSize());
+        minutes.setMaximumSize(minutes.getPreferredSize());
+        seconds.setMaximumSize(minutes.getPreferredSize());
+
+        timerSettings.add(hours);
+        timerSettings.add(Box.createHorizontalStrut(10));
+        timerSettings.add(seconds);
+        timerSettings.add(Box.createHorizontalStrut(10));
+        timerSettings.add(minutes);
+        timerSettings.add(Box.createVerticalGlue());
+
+        return timerSettings;
+    }
+
+    private JComboBox<String> createTimeComboBox(String[] values) {
+        JComboBox<String> comboBox = new JComboBox<>(values);
+        comboBox.setMaximumSize(comboBox.getPreferredSize());
+        return comboBox;
+    }
+
+    private String[] generateTimeUnits() {
+        String[] units = new String[MAX_TIME_UNIT];
+        for (int i = 0; i < MAX_TIME_UNIT; i++) {
+            units[i] = String.format("%02d", i);
+        }
+        return units;
+    }
+
+    private JLabel createPieceLabel(String imagePath) {
+        JLabel label = new JLabel();
+        try {
+            Image img = ImageIO.read(getClass().getResource(imagePath));
+            label.setIcon(new ImageIcon(img));
+        } catch (Exception e) {
+            System.err.println("⚠️ Required game file " + imagePath + " missing (" + e.getMessage() + ")");
+        }
+        return label;
+    }
+
+
 }
