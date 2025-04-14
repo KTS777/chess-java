60a11c9fcaa911d1e5270964438af289476518b2
KTS777 <ketitsinadze23@gmail.com>
2025-04-14 17:19:40 +0400

    Refactor PieceFactory for clarity and maintainability

--- src/model/PieceFactory.java
+++ src/model/PieceFactory.java
@@ -1,65 +1,58 @@
 package model;
 
 import model.pieces.*;
-
-
 import java.util.List;
-
 import static view.PieceImages.*;
 
 public class PieceFactory {
 
+    private static final int WHITE = 1;
+    private static final int BLACK = 0;
+    private static final int ROW_WHITE_BACK = 7;
+    private static final int ROW_WHITE_PAWN = 6;
+    private static final int ROW_BLACK_BACK = 0;
+    private static final int ROW_BLACK_PAWN = 1;
+
     public static King[] createStandardSetup(Square[][] board, List<Piece> Wpieces, List<Piece> Bpieces) {
+
         // Pawns
         for (int x = 0; x < 8; x++) {
-            Pawn bp = new Pawn(0, board[1][x], BPAWN);
-            Pawn wp = new Pawn(1, board[6][x], WPAWN);
-            board[1][x].put(bp); Bpieces.add(bp);
-            board[6][x].put(wp); Wpieces.add(wp);
+            placePiece(new Pawn(BLACK, board[ROW_BLACK_PAWN][x], BPAWN), board[ROW_BLACK_PAWN][x], Bpieces);
+            placePiece(new Pawn(WHITE, board[ROW_WHITE_PAWN][x], WPAWN), board[ROW_WHITE_PAWN][x], Wpieces);
         }
 
         // Queens
-        Queen bq = new Queen(0, board[0][3], BQUEEN);
-        Queen wq = new Queen(1, board[7][3], WQUEEN);
-        board[0][3].put(bq); Bpieces.add(bq);
-        board[7][3].put(wq); Wpieces.add(wq);
+        Queen bq = placePiece(new Queen(BLACK, board[ROW_BLACK_BACK][3], BQUEEN), board[ROW_BLACK_BACK][3], Bpieces);
+        Queen wq = placePiece(new Queen(WHITE, board[ROW_WHITE_BACK][3], WQUEEN), board[ROW_WHITE_BACK][3], Wpieces);
 
         // Kings
-        King bk = new King(0, board[0][4], BKING);
-        King wk = new King(1, board[7][4], WKING);
-        board[0][4].put(bk); Bpieces.add(bk);
-        board[7][4].put(wk); Wpieces.add(wk);
+        King bk = placePiece(new King(BLACK, board[ROW_BLACK_BACK][4], BKING), board[ROW_BLACK_BACK][4], Bpieces);
+        King wk = placePiece(new King(WHITE, board[ROW_WHITE_BACK][4], WKING), board[ROW_WHITE_BACK][4], Wpieces);
 
         // Rooks
-        Rook br1 = new Rook(0, board[0][0], BROOK);
-        Rook br2 = new Rook(0, board[0][7], BROOK);
-        Rook wr1 = new Rook(1, board[7][0], WROOK);
-        Rook wr2 = new Rook(1, board[7][7], WROOK);
-        board[0][0].put(br1); Bpieces.add(br1);
-        board[0][7].put(br2); Bpieces.add(br2);
-        board[7][0].put(wr1); Wpieces.add(wr1);
-        board[7][7].put(wr2); Wpieces.add(wr2);
+        placePiece(new Rook(BLACK, board[ROW_BLACK_BACK][0], BROOK), board[ROW_BLACK_BACK][0], Bpieces);
+        placePiece(new Rook(BLACK, board[ROW_BLACK_BACK][7], BROOK), board[ROW_BLACK_BACK][7], Bpieces);
+        placePiece(new Rook(WHITE, board[ROW_WHITE_BACK][0], WROOK), board[ROW_WHITE_BACK][0], Wpieces);
+        placePiece(new Rook(WHITE, board[ROW_WHITE_BACK][7], WROOK), board[ROW_WHITE_BACK][7], Wpieces);
 
         // Knights
-        Knight bk1 = new Knight(0, board[0][1], BKNIGHT);
-        Knight bk2 = new Knight(0, board[0][6], BKNIGHT);
-        Knight wk1 = new Knight(1, board[7][1], WKNIGHT);
-        Knight wk2 = new Knight(1, board[7][6], WKNIGHT);
-        board[0][1].put(bk1); Bpieces.add(bk1);
-        board[0][6].put(bk2); Bpieces.add(bk2);
-        board[7][1].put(wk1); Wpieces.add(wk1);
-        board[7][6].put(wk2); Wpieces.add(wk2);
+        placePiece(new Knight(BLACK, board[ROW_BLACK_BACK][1], BKNIGHT), board[ROW_BLACK_BACK][1], Bpieces);
+        placePiece(new Knight(BLACK, board[ROW_BLACK_BACK][6], BKNIGHT), board[ROW_BLACK_BACK][6], Bpieces);
+        placePiece(new Knight(WHITE, board[ROW_WHITE_BACK][1], WKNIGHT), board[ROW_WHITE_BACK][1], Wpieces);
+        placePiece(new Knight(WHITE, board[ROW_WHITE_BACK][6], WKNIGHT), board[ROW_WHITE_BACK][6], Wpieces);
 
         // Bishops
-        Bishop bb1 = new Bishop(0, board[0][2], BBISHOP);
-        Bishop bb2 = new Bishop(0, board[0][5], BBISHOP);
-        Bishop wb1 = new Bishop(1, board[7][2], WBISHOP);
-        Bishop wb2 = new Bishop(1, board[7][5], WBISHOP);
-        board[0][2].put(bb1); Bpieces.add(bb1);
-        board[0][5].put(bb2); Bpieces.add(bb2);
-        board[7][2].put(wb1); Wpieces.add(wb1);
-        board[7][5].put(wb2); Wpieces.add(wb2);
+        placePiece(new Bishop(BLACK, board[ROW_BLACK_BACK][2], BBISHOP), board[ROW_BLACK_BACK][2], Bpieces);
+        placePiece(new Bishop(BLACK, board[ROW_BLACK_BACK][5], BBISHOP), board[ROW_BLACK_BACK][5], Bpieces);
+        placePiece(new Bishop(WHITE, board[ROW_WHITE_BACK][2], WBISHOP), board[ROW_WHITE_BACK][2], Wpieces);
+        placePiece(new Bishop(WHITE, board[ROW_WHITE_BACK][5], WBISHOP), board[ROW_WHITE_BACK][5], Wpieces);
 
         return new King[]{ wk, bk };
+    }
+
+    private static <T extends Piece> T placePiece(T piece, Square square, List<Piece> collection) {
+        square.put(piece);
+        collection.add(piece);
+        return piece;
     }
 }
